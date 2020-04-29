63f476acd4e11155f7281f06d99c6e215a3ba169
Flo Jonckans <flo.j62@hotmail.fr>
2020-04-29 13:16:28 +0200

    ajout de css

--- resources/views/musique.blade.php
+++ resources/views/musique.blade.php
@@ -4,6 +4,7 @@
     Musique numéro : {{ $musique->id }}<br>
     Titre {{ $musique->nom }}<br>
     Ecouter : <audio id="musique" controls src="{{ $musique->fichier }}">Your browser does not support the <code>audio</code> element</audio><br>
+    <img class="imageMusique" src="{{ $musique->image }}"/>
     Style :  {{ $musique->style }}<br>
     Appartient à : <a href="/user/{{ $musique->utilisateur->id }}">{{ $musique->utilisateur->name }}</a>
 
--- resources/views/musiques.blade.php
+++ resources/views/musiques.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     tests d'affichage des musiques
-    @foreach($musiques as $m)
-        <li><a href="/musique/{{ $m->id }}">{{ $m->nom }}</a> </li>
-    @endforeach
+        @foreach($musiques as $m)
+            <li><a href="/musique/{{ $m->id }}">{{ $m->nom }}</a> </li>
+        @endforeach
 @endsection
