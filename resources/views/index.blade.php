380c3c78b799075d9aa31d17c6730c0b83b60be5
Flo Jonckans <flo.j62@hotmail.fr>
2020-05-07 15:05:30 +0200

    test avant THE apocalypse

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <form id="search" class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2 btn-sm" type="search" name="search" required placeholder="Recherche" aria-label="Search">
-            <button class="btn my-2 my-sm-0 btn-sm" style="color: white; background-color: #018d8a" type="submit">Valider</button>
-        </form>
-    </div>
     <h2>Écoutez gratuitement les dernières tendances de la communauté SoundCloud</h2>
     <div class="main-musiques-box">
     @foreach($musiques as $m)
@@ -19,5 +13,5 @@
     <h2>Merci de nous avoir écouté ! Inscrivez-vous dès maintenant.<br>
         Sauvegardez des titres, suivez des artistes et créez des playlists, tout cela gratuitement.</h2>
     <div class="content-input-button"><a class="input-button" href="{{ route('register') }}">Créer votre compte</a></div>
-    <div class="content-input-button">Vous avez déjà un compte ?   <a href="{{ route('login') }}">Se connecter</a></div>
+    <div class="content-input-button">Vous avez déjà un compte ? <a href="{{ route('login') }}">Se connecter</a></div>
 @endsection
